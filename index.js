dd63f1a24410d56c64a7c6ff6adbc6d36f817d83
Andrew Wei <andrewscwei@gmail.com>
2017-08-30 17:32:33 -0700

    Update jsdom

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // © Andrew Wei
 
 const chalk = require('chalk');
-const jsdom = require('mathjax-node/node_modules/jsdom');
+const JSDOM = require('jsdom').JSDOM;
 const MathJax = require('mathjax-node/lib/mj-page');
 
 const DEFAULT_CONFIG = {
@@ -13,23 +13,15 @@ module.exports = function(htmlString, options) {
   options = Object.assign(DEFAULT_CONFIG, options || {});
 
   return new Promise((resolve, reject) => {
-    jsdom.env({
-      html: htmlString,
-      done: (err, window) => {
-        if (err) {
-          console.error(`${chalk.cyan('[mathjax-dom]')} ${chalk.red(err)}`);
-          return reject(err);
-        }
-  
-        MathJax.start();
-        MathJax.typeset(Object.assign({
-          html: window.document.body.innerHTML
-        }, options), result => {
-          window.document.body.innerHTML = result.html;
-          const html = '<!DOCTYPE html>\n' + window.document.documentElement.outerHTML.replace(/^(\n|\s)*/, '');
-          resolve(html);
-        });
-      }
+    const window = new JSDOM(htmlString).window;
+    
+    MathJax.start();
+    MathJax.typeset(Object.assign({
+      html: window.document.body.innerHTML
+    }, options), result => {
+      window.document.body.innerHTML = result.html;
+      const html = '<!DOCTYPE html>\n' + window.document.documentElement.outerHTML.replace(/^(\n|\s)*/, '');
+      resolve(html);
     });
   });
 }
